b326611b0a7433dc96280a8a2707894b2b8e0be8
Jelle <jellestege@gmail.com>
2015-08-07 20:21:50 +0200

    Added full implementation of PriorityQueue
    Full implementation of PriorityQueue added. Also added tests for
    nearly all methods. Also updated README with additional documentation.

--- README.md
+++ README.md
@@ -7,31 +7,25 @@ PriorityQueue is a Javascript implementation of a min PriorityQueue.
 var queue = new PriorityQueue(options)
 ```
 The constructor checks if the given options object contains one of the following fields:
-* capacity:
-  **number** Defines the maximum capacity of the queue. Defaults to the maximum safe integer in
-  javascript, which is 2<sup>53</sup>=9007199254740991. When the queue is at maximum length, it
-  is no longer possible to add elements to it.
 * comperator:
   **function** The comparator to use when comparing elements in the queue. This is used for
   sorting the queue. When adding elements which can not be easily compared, supply a comparator
-  which is able to compare the given object. For example, when you have a data type which looks
-  like the following:
+  which is able to compare the given object. Not using a right comparator will result in
+  undefined behaviour, possibly not giving the right results. Remember to take null or undefined
+  values into account. Default comparator is as follows:
 ```javascript
-var obj = {
-  "value": 4
-};
-```
-  You might want to add a comparator which looks like the following:
-```javascript
-function comparator(a, b) {
-  return a.value - b.value;
+function compare(a, b) {
+    if (typeof a == 'undefined' || a == null) {
+        return 1;
+    }
+    if (typeof b == 'undefined' || b == null) {
+        return -1;
+    }
+    return a - b;
 }
 ```
-  Not using a right comparator will result in undefined behaviour, possibly not giving the right
-  results.
 * data:
-  **array** The initial dataset to use to create a priority queue from. Accepts an array and
-  will add each element seperately.
+  **array** The initial dataset to use to create a priority queue from. Accepts an array.
 * equals:
   **function** The equals function to use. This is used for checking whether or not an element is
    in the queue. By default, equals will check if 2 values are equal by a simple `a == b` check.
@@ -53,6 +47,26 @@ function comparator(a, b) {
   complete binary tree.
 * `contains(obj)`:
   Checks if the given `obj` is present in the queue.
-
+* `getData(reference)`:
+  Returns the internal queue array. Mostly usable for debugging purposes. The reference argument
+  indicates whether the returned array is passed by reference or if a copy is returned. This
+  argument is optional. Note that this will not return a sorted array, it will return the
+  internal representation of the queue.
+* `setData(data)`:
+  Sets the data of the queue. Will return to a complete binary tree after adding.
+* `size()`:
+  Returns the amount of objects in the queue, is not equal to getArray().length. The internal
+  array might be a lot longer when objects are polled/removed from the queue.
+* `clone()`:
+  Copies the queue by value into a new PriorityQueue object with the same values as the original
+  queue.
+* `setComparator(func)`:
+  Sets the comparator to be used.
+* `getComparator()`:
+  Returns the comparator used.
+* `setEquals(func)`
+  Sets the equals function to be used.
+* `getEquals()`
+  Returns the equals function used.
 ## License
 This code is released in the public domain. I do not claim any ownership and waive all my rights.
--- priorityqueue.js
+++ priorityqueue.js
@@ -21,11 +21,19 @@ if (!('jstege1206' in net)) {
 
   /**
    * Default comparator, returns < 0 if b > a, > 0 if a > b and 0 if a == b.
+   * Null values are always bigger than non null values, resulting in null
+   * values being sorted to the end.
    * @param {*} a The first object.
    * @param {*} b the second object.
    * @return {number} The value representing the comparison.
    */
   var DEFAULT_COMPARATOR = function(a, b) {
+    if (a == null) {
+      return 1;
+    }
+    if (b == null) {
+      return -1;
+    }
     return a - b;
   };
 
@@ -33,30 +41,11 @@ if (!('jstege1206' in net)) {
    * Default equals function, returns true if a == b. False if otherwise.
    * @param {*} a First object.
    * @param {*} b Second object.
-   * @returns {boolean} True if a and b are equal, false if otherwise.
+   * @return {boolean} True if a and b are equal, false if otherwise.
    */
   var DEFAULT_EQUALS = function(a, b) {
     return a == b;
   };
-
-  /**
-   * Default capacity, queue can not handle more elements than this.
-   * Defaults to the maximum safe integer in Javascript (2^53 - 1).
-   * @type {Number}
-   */
-  var DEFAULT_CAPACITY = (Math.pow(2, 53) - 1)|0;
-
-  /**
-   * The internal queue array.
-   * @type {Array}
-   */
-  var queue;
-  /**
-   * The queue length, can not use queue.length since it does not represent
-   * the actual queue length (adding and removing an element will still
-   * result in queue.length == 1). Maintaining it manually will fix this.
-   */
-  var queueLength;
 
   /**
    * PriorityQueue constructor, constructs a new PriorityQueue. Options are a
@@ -67,37 +56,31 @@ if (!('jstege1206' in net)) {
    *  - capacity: The capacity of the queue, defines the maximum size of the
    *      queue. When the queue exceeds this size any new additions will
    *      throw an error.
-   *  - data: The initial data to be used for the queue. Only accepts arrays.
+   *  - data: The initial data to be used for the queue. Accepts any
+   *  iterable object.
    * @param {*} options options object,
    * @constructor
    */
   function PriorityQueue(options) {
-    queue = [];
-    queueLength = 0;
-    if ('comparator' in options && typeof options.comparator == 'function') {
+    this.queue = [];
+    this.queueLength = 0;
+    if (typeof options != 'undefined' && 'comparator' in options &&
+        typeof options.comparator == 'function') {
       this.comparator = options.comparator;
     } else {
       this.comparator = DEFAULT_COMPARATOR;
     }
 
-    if ('equals' in options && typeof options.equals == 'function') {
+    if (typeof options != 'undefined' && 'equals' in options &&
+        typeof options.equals == 'function') {
       this.equals = options.equals;
     } else {
       this.equals = DEFAULT_EQUALS;
     }
 
-    if ('capacity' in options &&
-      typeof options.capacity == 'number' &&
-      options.capacity >= 0) {
-      this.capacity = options.capacity;
-    } else {
-      this.capacity = DEFAULT_CAPACITY;
-    }
-
-    if ('data' in options && typeof options.data == 'object' && options.data[0]) {
-      options.data.forEach(function(val) {
-        this.offer(val);
-      });
+    if (typeof options != 'undefined' && 'data' in options &&
+        typeof options.data == 'object') {
+      this.setData(options.data, options.data.length);
     }
   }
 
@@ -105,57 +88,49 @@ if (!('jstege1206' in net)) {
    * Clears whole queue, removing all elements from it.
    */
   PriorityQueue.prototype.clear = function() {
-    queue = [];
-    queueLength = 0;
+    this.queue = [];
+    this.queueLength = 0;
   };
 
   /**
    * Returns the first object in the queue, without removing it.
-   * @returns {*} The first object in the queue.
+   * @return {*} The first object in the queue.
    */
   PriorityQueue.prototype.peek = function() {
-    if (queueLength == 0) {
+    if (this.queueLength == 0) {
       return null;
     }
-    return queue[0];
+    return this.queue[0];
   };
 
   /**
    * Returns the first object in the queue and removes it from the queue.
-   * @returns {*} The first object in the queue.
+   * @return {*} The first object in the queue.
    */
   PriorityQueue.prototype.poll = function() {
-    if (queueLength == 0) {
+    if (this.queueLength == 0) {
       return null;
     }
-    var s = --queueLength;
-    var result = queue[0];
-    var x = queue[s];
-    queue[s] = null;
+    var s = --this.queueLength;
+    var result = this.queue[0];
+    var x = this.queue[s];
+    this.queue[s] = null;
     if (s != 0) {
-      _siftDown(0, x);
+      _siftDown(0, x, this.queue, this.queueLength, this.comparator);
     }
     return result;
   };
 
   /**
-   * Adds an object to the queue. Returns false if this is not possible due
-   * to a full capacity.
-   * @param obj The object to store in the queue.
-   * @returns {boolean} True if the object is added, false if otherwise.
+   * Adds an object to the queue.
+   * @param {*} obj The object to store in the queue.
    */
   PriorityQueue.prototype.offer = function(obj) {
-    if (queueLength == this.capacity) {
-      //Throw error or something.
-      return false;
+    this.queueLength++;
+    if (this.queueLength - 1 == 0) {
+      this.queue[0] = obj;
     } else {
-      queueLength++;
-      if (queueLength - 1 == 0) {
-        queue[0] = obj;
-      } else {
-        _siftUp(queueLength - 1, obj);
-      }
-      return true;
+      _siftUp(this.queueLength - 1, obj, this.queue, this.comparator);
     }
   };
 
@@ -163,91 +138,201 @@ if (!('jstege1206' in net)) {
    * Removes an item from the queue by using the given object as identifier.
    * Will use the equals function of this PriorityQueue object to compare
    * objects.
-   * @param obj The object to remove.
-   * @returns {boolean} False if object was not found, true if it was found
+   * @param {*} obj The object to remove.
+   * @return {boolean} False if object was not found, true if it was found.
    * and removed.
    */
   PriorityQueue.prototype.remove = function(obj) {
-    var i = _indexOf(obj, this.equals);
+    var i = _indexOf(obj, this.queue, this.queueLength, this.equals);
     if (i == -1) {
       return false;
     } else {
-      _removeAt(i);
+      this.queueLength--;
+      _removeAt(i, this.queue, this.queueLength, this.comparator, this.equals);
       return true;
     }
   };
 
   /**
    * Returns whether or not the given object is in the queue or not.
-   * @param obj The object to find.
-   * @returns {boolean} True if the queue contains the object, false if
+   * @param {*} obj The object to find.
+   * @return {boolean} True if the queue contains the object, false if
    * otherwise.
    */
   PriorityQueue.prototype.contains = function(obj) {
-    return _indexOf(obj, this.equals) != -1;
+    return _indexOf(obj, this.queue, this.queueLength, this.equals) != -1;
+  };
+
+  /**
+   * Returns the internal queue array. Argument indicates whether the
+   * returned array is a copy of the array, or the array is returned by
+   * reference.
+   * @param {boolean} reference true if the returned array should refer to
+   * the internal queue, false or not present if it should be a copy.
+   * @return {Array} (Possibly a copy of) the internal queue array.
+   */
+  PriorityQueue.prototype.getData = function(reference) {
+    if (reference) {
+      return this.queue;
+    }
+    return this.queue.slice();
+  };
+
+  /**
+   * Sets the data for this queue. Will automatically convert to a complete
+   * binary tree.
+   * @param {Array} data The array which should be used for a PriorityQueue.
+   * @param {number} size Optional, the size of the array. When not present,
+   * will use the length of the array. This is be needed when the data to
+   * set comes from another PriorityQueue.
+   */
+  PriorityQueue.prototype.setData = function(data, size) {
+    var arr = [];
+    data.forEach(function(val) {
+      arr.push(val);
+    });
+    this.queue = arr;
+    this.queueLength = size ? size : data.length;
+    this.heapify();
+  };
+
+  /**
+   * Sets the comparator to use.
+   * @param {function} func The comparator to use.
+   */
+  PriorityQueue.prototype.setComparator = function(func) {
+    if (typeof func == 'function') {
+      this.comparator = func;
+    }
+  };
+
+  /**
+   * Returns the comparator used in this queue.
+   * @return {function} The comparator used in this queue.
+   */
+  PriorityQueue.prototype.getComparator = function() {
+    return this.comparator;
+  };
+
+  /**
+   * Sets the equals function to use.
+   * @param {function} func The equals function to use.
+   */
+  PriorityQueue.prototype.setEquals = function(func) {
+    if (typeof func == 'function') {
+      this.equals = func;
+    }
+  };
+
+  /**
+   * Returns the equals function used in this queue.
+   * @return {function} The equals function used in this queue.
+   */
+  PriorityQueue.prototype.getEquals = function() {
+    return this.equals;
+  };
+
+  /**
+   * Returns the amount of elements in the queue.
+   * @return {number}
+   */
+  PriorityQueue.prototype.size = function() {
+    return this.queueLength;
+  };
+
+  /**
+   * Restores the queue to a complete binary tree.
+   */
+  PriorityQueue.prototype.heapify = function() {
+    for (var i = ((this.queueLength / 2) | 0); i >= 0; i--) {
+      _siftDown(i, this.queue[i], this.queue, this.queueLength,
+          this.comparator);
+    }
+  };
+
+  /**
+   * Returns a copy of this queue.
+   * @return {PriorityQueue} The clone.
+   */
+  PriorityQueue.prototype.clone = function() {
+    return new net.jstege1206.PriorityQueue({
+      comparator: this.comparator,
+      equals: this.equals,
+      data: this.queue.slice()
+    });
   };
 
   /**
    * Restores the complete binary tree by sifting down the given object.
    * @param {Number} pos The position to start sifting down from.
+   * @param {Array} queue The queue to use.
+   * @param {number} queueLength The length of the queue.
+   * @param {function} comparator The comparator to use.
    * @param {*} obj The object to sift down.
    * @private
    */
-  function _siftDown(pos, obj) {
-    var half = queueLength / 2;
+  function _siftDown(pos, obj, queue, queueLength, comparator) {
+    var half = (queueLength / 2) | 0;
     while (pos < half) {
       var lChild = pos * 2 + 1;
       var c = queue[lChild];
       var rChild = lChild + 1;
-      if (rChild < queueLength && this.comparator(c, queue[rChild]) > 0) {
+      if (rChild < queueLength && comparator(c, queue[rChild]) > 0) {
         lChild = rChild;
         c = queue[lChild];
       }
-      if (this.comparator(obj, c) <= 0) {
+      if (comparator(obj, c) <= 0) {
         break;
       }
       queue[pos] = c;
       pos = lChild;
     }
+    queue[pos] = obj;
   }
 
   /**
    * Restores the complete binary tree by sifting up the given object.
    * @param {Number} pos The position to start sifting up from.
    * @param {*} obj The object to sift up.
+   * @param {Array} queue The queue to use.
+   * @param {function} comparator The comparator to use.
    * @private
    */
-  function _siftUp(pos, obj) {
+  function _siftUp(pos, obj, queue, comparator) {
     while (pos > 0) {
-      var parent = ((pos - 1) / 2)|0;
+      var parent = ((pos - 1) / 2) | 0;
       var c = queue[parent];
-      if (this.comparator(obj, c) >= 0) {
+      if (comparator(obj, c) >= 0) {
         break;
       }
       queue[pos] = c;
       pos = parent;
     }
+    queue[pos] = obj;
   }
 
   /**
    * Remove an element at the given position. Will restore the complete binary
    * tree after removing the element.
-   * @param pos The position from which to remove the element.
-   * @returns {*} The element which was removed, or null if the element was
+   * @param {number} pos The position from which to remove the element.
+   * @param {Array} queue The queue to use.
+   * @param {number} queueLength The length of the queue.
+   * @param {function} comparator The comparator to use.
+   * @param {function} equals The equals function to use.
+   * @return {*} The element which was removed, or null if the element was
    * not found.
    * @private
    */
-  function _removeAt(pos) {
-    var s = queueLength - 1;
-    if (s == pos) {
-      queue[s] = null;
+  function _removeAt(pos, queue, queueLength, comparator, equals) {
+    if (queueLength == pos) {
+      queue[queueLength] = null;
     } else {
-      var moved = queue[s];
-      queue[s] = null;
-      _siftDown(pos, moved);
-      if (this.equals(queue[pos], moved)) {
-        _siftUp(pos, moved);
-        if (!this.equals(queue[pos], moved)) {
+      var moved = queue[queueLength];
+      queue[queueLength] = null;
+      _siftDown(pos, moved, queue, queueLength, comparator);
+      if (equals(queue[pos], moved)) {
+        _siftUp(pos, moved, queue, comparator);
+        if (!equals(queue[pos], moved)) {
           return moved;
         }
       }
@@ -257,13 +342,15 @@ if (!('jstege1206' in net)) {
 
   /**
    * Returns the position of the given object in the queue.
-   * @param obj The object to find.
-   * @param equals The equals function to use.
-   * @returns {number} The position in the queue the element is at, -1 if
+   * @param {*} obj The object to find.
+   * @param {Array} queue The queue to search.
+   * @param {number} queueLength Length of the queue.
+   * @param {function} equals The equals function to use.
+   * @return {number} The position in the queue the element is at, -1 if
    * not found.
    * @private
    */
-  function _indexOf(obj, equals) {
+  function _indexOf(obj, queue, queueLength, equals) {
     if (obj != null) {
       for (var i = 0; i < queueLength; i++) {
         if (equals(obj, queue[i])) {
